3bc8f6ffe3ff2b07a5ec86c25e80d95b7ed759c9
Park YuongHo <hexdrinker2020@gmail.com>
2025-03-29 23:59:13 +0900

    feat: createElement, eventManager 기본 기능 완성하여 렌더링 테스트

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,5 +1,59 @@
 import { addEvent } from "./eventManager";
 
-export function createElement(vNode) {}
+export function createElement(vNode) {
+  if (vNode === null || vNode === undefined || typeof vNode === "boolean")
+    return "";
 
-function updateAttributes($el, props) {}
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return document.createTextNode(vNode);
+  }
+
+  if (Array.isArray(vNode)) {
+    const fragment = document.createDocumentFragment();
+    vNode.children.forEach((child) => {
+      const childElement = createElement(child);
+      fragment.appendChild(childElement);
+    });
+
+    return fragment;
+  }
+
+  if (typeof vNode.type === "function") {
+    const componentVNode = vNode.type({
+      ...vNode.props,
+      children: vNode.children,
+    });
+    return createElement(componentVNode);
+  }
+  const element = document.createElement(vNode.type);
+
+  if (vNode.props) {
+    updateAttributes(element, vNode.props);
+  }
+
+  vNode.children.forEach((child) => {
+    if (!child) return;
+    const childElement = createElement(child);
+    if (childElement) {
+      element.appendChild(childElement);
+    }
+  });
+
+  return element;
+}
+
+function updateAttributes($el, props) {
+  const propsKeys = Object.keys(props);
+
+  for (const key of propsKeys) {
+    if (typeof props[key] === "function") {
+      console.log(key);
+      const eventType = key.toLowerCase().substring(2);
+      addEvent($el, eventType, props[key]);
+    } else if (key === "className") {
+      $el.setAttribute("class", props[key]);
+    } else {
+      $el.setAttribute(key, props[key]);
+    }
+  }
+}
--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,5 +1,43 @@
-export function setupEventListeners(root) {}
+const eventMap = new Map();
 
-export function addEvent(element, eventType, handler) {}
+export function setupEventListeners(root) {
+  eventMap.forEach((handlers, eventType) => {
+    root.addEventListener(eventType, (e) => {
+      let target = e.target;
 
-export function removeEvent(element, eventType, handler) {}
+      while (target && target !== root) {
+        handlers.forEach(({ element, handler }) => {
+          if (element === target) {
+            handler(e);
+          }
+        });
+        target = target.parentElement;
+      }
+    });
+  });
+}
+
+export function addEvent(element, eventType, handler) {
+  if (!eventMap.has(eventType)) {
+    eventMap.set(eventType, []);
+  }
+
+  eventMap.get(eventType).push({ element, handler });
+}
+
+export function removeEvent(element, eventType, handler) {
+  if (!eventMap.has(eventType)) return;
+
+  const handlers = eventMap.get(eventType);
+  const index = handlers.findIndex((item) => {
+    return item.element === element && item.handler === handler;
+  });
+
+  if (index !== -1) {
+    handlers.splice(index, 1);
+  }
+
+  if (!handlers.length) {
+    eventMap.delete(eventType);
+  }
+}
