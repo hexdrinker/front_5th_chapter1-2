e6e3da551c02dbb303354c4b51a34bab0a98ec6e
Park YuongHo <hexdrinker2020@gmail.com>
2025-03-30 13:06:48 +0900

    feat; renderElement, updateElement 메소드 구현

--- src/lib/updateElement.js
+++ src/lib/updateElement.js
@@ -1,6 +1,12 @@
-import { addEvent, removeEvent } from "./eventManager";
-import { createElement } from "./createElement.js";
+import { cleanupElementEvents } from "./eventManager";
+import { createElement } from "./createElement";
 
-function updateAttributes(target, originNewProps, originOldProps) {}
+// function updateAttributes(target, originNewProps, originOldProps) {}
 
-export function updateElement(parentElement, newNode, oldNode, index = 0) {}
+export function updateElement(parentElement, newNode, oldElement) {
+  // 이벤트 제거
+  cleanupElementEvents(oldElement);
+
+  const newElement = createElement(newNode);
+  parentElement.replaceChild(newElement, oldElement);
+}
