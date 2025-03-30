d2bef8a526b79b2d56a3926788553b3c8c103463
Park YuongHo <hexdrinker2020@gmail.com>
2025-03-30 13:04:47 +0900

    fix: createElement 메소드 개선

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,30 +1,45 @@
 import { addEvent } from "./eventManager";
 
-export function createElement(vNode) {
-  if (vNode === null || vNode === undefined || typeof vNode === "boolean")
-    return "";
+/*
+  1. vNode가 null, undefined, boolean 일 경우, 빈 텍스트 노드를 반환합니다.
+  2. vNode가 문자열이나 숫자면 텍스트 노드를 생성하여 반환합니다.
+  3. vNode가 배열이면 DocumentFragment를 생성하고 각 자식에 대해 createElement를 재귀 호출하여 추가합니다.
+  4. 위 경우가 아니면 실제 DOM 요소를 생성합니다:
+    - vNode.type에 해당하는 요소를 생성
+    - vNode.props의 속성들을 적용 (이벤트 리스너, className, 일반 속성 등 처리)
+    - vNode.children의 각 자식에 대해 createElement를 재귀 호출하여 추가
+*/
 
+export function createElement(vNode) {
+  // Case 1
+  if (vNode === null || vNode === undefined || typeof vNode === "boolean")
+    return document.createTextNode("");
+
+  // Case 2
   if (typeof vNode === "string" || typeof vNode === "number") {
     return document.createTextNode(vNode);
   }
 
+  // Case 3
   if (Array.isArray(vNode)) {
     const fragment = document.createDocumentFragment();
-    vNode.children.forEach((child) => {
-      const childElement = createElement(child);
-      fragment.appendChild(childElement);
-    });
+
+    if (vNode.children) {
+      vNode.children.forEach((child) => {
+        const childElement = createElement(child);
+        fragment.appendChild(childElement);
+      });
+    } else {
+      vNode.forEach((child) => {
+        const childElement = createElement(child);
+        fragment.appendChild(childElement);
+      });
+    }
 
     return fragment;
   }
 
-  if (typeof vNode.type === "function") {
-    const componentVNode = vNode.type({
-      ...vNode.props,
-      children: vNode.children,
-    });
-    return createElement(componentVNode);
-  }
+  // Case 4
   const element = document.createElement(vNode.type);
 
   if (vNode.props) {
@@ -47,7 +62,6 @@ function updateAttributes($el, props) {
 
   for (const key of propsKeys) {
     if (typeof props[key] === "function") {
-      console.log(key);
       const eventType = key.toLowerCase().substring(2);
       addEvent($el, eventType, props[key]);
     } else if (key === "className") {
