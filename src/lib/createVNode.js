46c3a47f3921ad032700bf550a67d13dc0c39f55
Park YuongHo <hexdrinker2020@gmail.com>
2025-03-30 13:03:40 +0900

    fix: vNode 생성 시 falsy 값 filtering

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,7 +1,16 @@
+/*
+  1. type, props, ...children을 매개변수로 받는 함수를 작성하세요.
+  2. 반환값은 { type, props, children } 형태의 객체여야 합니다.
+*/
+
 export function createVNode(type, props, ...children) {
   return {
     type,
     props,
-    children: children.flat(Infinity),
+    children: children
+      .flat(Infinity)
+      .filter(
+        (child) => child !== null && child !== undefined && child !== false,
+      ),
   };
 }
